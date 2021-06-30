6f9eac376b65acea63f0509ecfcecf7451d9dd6a
Shahzad Waris <shahzadwaris81@gmail.com>
2021-06-30 22:53:13 +0500

    design changes

---
+++ resources/views/welcome/index.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.app')
+@section('content')
+{{-- header  --}}
+<div class="container-fluid p-0">
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+            <div class="row mx-0">
+                <div class="col-12 w-100 text-center">
+                    <p class="lead">Share your personal style. Stay inspired.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+{{-- feed --}}
+
+<div class="container">
+    <div class="row">
+        {{-- feed-div --}}
+        <div class="col-md-8">
+            <p class="text border-bottom">HYPE the looks you like, and +FAN the people you like to personalize your
+                feed.</p>
+            <p class="text-dark font-weight-bold">Hot looks, by diversity</p>
+            <div class="row filters">
+                <div class="col-12">
+                    Need to work
+                </div>
+            </div>
+        </div>
+
+
+        <div class="col-md-4"></div>
+    </div>
+</div>
+
+@endsection
